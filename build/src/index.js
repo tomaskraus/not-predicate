fec8b33b92131740045345634bb16a3c8a4e5bdc
Tomas Kraus <tomas.kraus.dev@gmail.com>
2022-11-15 21:55:48 +0100

    chore(tsdoc): complete function docs

--- build/src/index.js
+++ build/src/index.js
@@ -1,13 +1,18 @@
 "use strict";
-/*
+/**
  * Predicate negation. Suitable for RxJS, IxJS, Array.filter and others.
+ *
+ * @public
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.not = void 0;
 /**
+ * Predicate that negates a result of its predicate argument.
+ * @see {@link PredicateType}
+ *
  * @template T The type of input value.
- * @param {(value: T, index: number) => boolean} predicate Original predicate. A function to test a value for condition. Predicate can also accept an index argument, such as array position.
- * @returns {(value: T, index: number) => boolean} A predicate that returns a negation of the original predicate.
+ * @param {(value: T, index: number) => boolean} predicate A predicate argument. A function to test a value for condition. Predicate can also accept an index argument, such as array position.
+ * @returns {(value: T, index: number) => boolean} A predicate that negates a result of its predicate argument.
  */
 function not(predicate) {
     return (value, index) => !predicate(value, index);
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-/*
+/**
  * Predicate negation. Suitable for RxJS, IxJS, Array.filter and others.
+ *
+ * @public
  */
 
 /**
+ * A predicate type. Predicate is a function, that accepts some value and returns a boolean value, based on its condition.
  *
+ * @template T The type of input value.
+ * @param value A value provided for the condition. It is meant to be a value's order in some sequence, such as a value's index for the value in an array.
+ * @param index A numerical value provided for the condition.
+ *
+ * @returns True, if value (and index) meet the predicate's condition.
  */
 export type PredicateType<T> = (value: T, index: number) => boolean;
 
 /**
+ * Predicate that negates a result of its predicate argument.
+ * @see {@link PredicateType}
+ *
  * @template T The type of input value.
- * @param {(value: T, index: number) => boolean} predicate Original predicate. A function to test a value for condition. Predicate can also accept an index argument, such as array position.
- * @returns {(value: T, index: number) => boolean} A predicate that returns a negation of the original predicate.
+ * @param {(value: T, index: number) => boolean} predicate A predicate argument. A function to test a value for condition. Predicate can also accept an index argument, such as array position.
+ * @returns {(value: T, index: number) => boolean} A predicate that negates a result of its predicate argument.
  */
 export function not<T>(predicate: PredicateType<T>): PredicateType<T> {
   return (value: T, index: number) => !predicate(value, index);
