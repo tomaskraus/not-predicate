42521eb3157c62e6077e35db344192c7223e1999
Tomas Kraus <tomas.krausik@gmail.com>
2022-11-24 01:04:25 +0100

    Ptype and this (#3)
    * feat: handles `this` object correctly
    
    * 1.1.0 - handles  object correctly
    
    * 1.2.0 - handles -this- object correctly
    
    * refactor: rename a predicate type
    
    * 2.0.0 - rename the predicate type
    
    Co-authored-by: Tomas Kraus <tomas.kraus.dev@gmail.com>

--- build/src/index.js
+++ build/src/index.js
@@ -8,14 +8,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.not = void 0;
 /**
  * Predicate that negates a result of its predicate argument.
- * @see {@link PredicateType}
+ * @see {@link TPredicate}
  *
  * @template T The type of input value.
  * @param {(value: T, index: number) => boolean} predicate A predicate argument. A function to test a value for condition. Predicate can also accept an index argument, such as array position.
  * @returns {(value: T, index: number) => boolean} A predicate that negates a result of its predicate argument.
  */
 function not(predicate) {
-    return (value, index) => !predicate(value, index);
+    return function (value, index) {
+        return !predicate.call(this, value, index);
+    };
 }
 exports.not = not;
 //# sourceMappingURL=index.js.map
--- build/test/index.test.js
+++ build/test/index.test.js
@@ -3,6 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("../src/index");
 const isEven = (n) => n % 2 === 0;
 const isPositionEven = (_, i) => i % 2 === 1;
+const customThis = {
+    myProp: 'hereIAm',
+};
+function isIndexEqualThreeAndThisObjectHasMyPropDefined(_, index) {
+    if (this) {
+        return index === 3 && this['myProp'] === 'hereIAm';
+    }
+    return false;
+}
 test('Negates the predicate.', () => {
     expect([1, 2, 2, 3].map((0, index_1.not)(isEven))).toEqual([true, false, false, true]);
 });
@@ -17,5 +26,10 @@ test('Negates the predicate that uses index argument.', () => {
 test('Double negation cancels itself.', () => {
     const nums = [1, 2, 2, 3];
     expect(nums.map((0, index_1.not)((0, index_1.not)(isEven)))).toEqual(nums.map(isEven));
+});
+test('deals with this', () => {
+    // without explicit this
+    expect([2, 4, 3, 5, 0, 3, 3].filter((0, index_1.not)(isIndexEqualThreeAndThisObjectHasMyPropDefined))).toEqual([2, 4, 3, 5, 0, 3, 3]);
+    expect([2, 4, 3, 5, 0, 3, 3].filter((0, index_1.not)(isIndexEqualThreeAndThisObjectHasMyPropDefined), customThis)).toEqual([2, 4, 3, 0, 3, 3]);
 });
 //# sourceMappingURL=index.test.js.map
